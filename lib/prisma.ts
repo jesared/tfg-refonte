f98969d8976ca68b538f1436455eb137a051115f
jerome jesared <jesared@gmail.com>
2026-02-25 21:21:16 +0100

    push

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  log: ["query", "error", "warn"],
-});
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,7 +16,7 @@ export const authOptions: NextAuthOptions = {
   ],
 
   session: {
-    strategy: "jwt",
+    strategy: "database",
   },
 
   callbacks: {
