33429c1bf99e44c809b371cdacbfd25e0e8f16f8
jerome jesared <jesared@gmail.com>
2026-02-25 20:56:55 +0100

    u^p

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,11 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
